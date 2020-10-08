f1a83b25318f09546f43b322ead9a72219c9cea2
Bayma Bruno <baymabruno@gmail.com>
2020-10-07 23:29:10 -0300

    titulo

---
+++ src/components/Titulo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+import estilo from './estilo';
+
+export default (props) => (
+  <>
+    <Text style={estilo.txtG}>{props.principal}</Text>
+    <Text>{props.secundario}</Text>
+  </>
+);
