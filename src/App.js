2ae01639bed2495c77944b430e394b03b6c5b678
Bayma Bruno <baymabruno@gmail.com>
2020-10-11 18:13:29 -0300

    34. Renderização Condicional #02

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import Familia from './components/relacao/Familia';
-import Membro from './components/relacao/Membro';
+import UsuarioLogado from './components/UsuarioLogado';
 
 export default () => (
   <SafeAreaView style={style.App}>
+    <UsuarioLogado usuario={{ nome: 'Gui', email: 'gui@gui.com' }} />
+    <UsuarioLogado usuario={{ nome: 'Ana' }} />
+    <UsuarioLogado usuario={{ email: 'carlos@empresa.com' }} />
+    {/*
+  
     <Familia>
       <Membro nome="Bia" sobrenome="Arruda" />
       <Membro nome="Carlos" sobrenome="Arruda" />
@@ -15,7 +19,6 @@ export default () => (
       <Membro nome="Gui" sobrenome="Silva" />
       <Membro nome="Rebeca" sobrenome="Silva" />
     </Familia>
-    {/*
     <ParImpar num={3} />
     <Diferenciar />
     <ContadorV2 />
